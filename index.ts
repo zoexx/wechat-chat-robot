6ba1484331e595f1618c33bda827f500d6916657
zoexx <296852455@qq.com>
2017-06-02 21:45:20 +0800

    更新自动加群

--- index.ts
+++ index.ts
@@ -22,6 +22,7 @@ import {
   Contact,
   Wechaty,
   Message,
+  Room,
   FriendRequest,
   log,
 }           from 'wechaty'
@@ -32,6 +33,8 @@ import updateContactData from './tasks/updateContactData'
 const welcome = `
 Please wait... I'm trying to login in...
 `
+
+const HELPER_CONTACT_NAME = '刘文超'
 
 console.log(welcome)
 const bot = Wechaty.instance({ profile: Config.DEFAULT_PROFILE })
@@ -74,14 +77,82 @@ bot
       console.log(`new friendship confirmed with ${contact.name()}`)
   }
 })
-.on('message', (message: Message) => {
-  console.log(`message ${message} received`)
-  // console.log(`message type ${message.type()}`)
-  // console.log(`message type ${message.typeApp()}`)
-  console.log(`message type ${message.getSenderString()}`)
-  console.log(`message type ${message.getContentString()}`)
-  if( message.content() === '666' ){
-    message.say('welcome')
+.on('message', function (this, message) {
+  const room    = message.room()
+  const sender  = message.from()
+  const content = message.content()
+
+  console.log((room ? '[' + room.topic() + ']' : '')
+              + '<' + sender.name() + '>'
+              + ':' + message.toStringDigest(),
+  )
+
+  if (message.self()) {
+    return
+  }
+  /**
+   * `ding` will be the magic(toggle) word:
+   *  1. say ding first time, will got a room invitation
+   *  2. say ding in room, will be removed out
+   */
+  if (/^666$/i.test(content)) {
+
+    /**
+     *  in-room message
+     */
+    if (room) {
+      if (/^666/i.test(room.topic())) {
+        /**
+         * move contact out of room
+         */
+        getOutRoom(sender, room)
+      }
+
+    /**
+     * peer to peer message
+     */
+    } else {
+
+      /**
+       * find room name start with "666"
+       */
+      Room.find({ topic: /^666/i })
+          .then(dingRoom => {
+
+            /**
+             * room found
+             */
+            if (dingRoom) {
+              log.info('Bot', 'onMessage: got dingRoom: %s', dingRoom.topic())
+
+              /**
+               * speaker is already in room
+               */
+              if (dingRoom.has(sender)) {
+                log.info('Bot', 'onMessage: sender has already in')
+                sender.say('no need to 666 again, because you are already in the room')
+
+              /**
+               * put speaker into room
+               */
+              } else {
+                log.info('Bot', 'onMessage: add sender(%s) to dingRoom(%s)', sender.name(), dingRoom.topic())
+                sender.say('ok, I will put you in 666 room!')
+                putInRoom(sender, dingRoom)
+              }
+
+            /**
+             * room not found
+             */
+            } else {
+              log.info('Bot', 'onMessage: dingRoom not found, try to create one')
+              sender.say('666 room was dissolved!')
+            }
+          })
+          .catch(e => {
+            log.error(e)
+          })
+    }
   }
 })
 .on('room-join', function(this, room, inviteeList, inviter) {
@@ -122,4 +193,68 @@ bot.init()
 async function main() {
   updateContactData()
   // updateRoomData()
+}
+
+
+function putInRoom(contact, room) {
+  log.info('Bot', 'putInRoom(%s, %s)', contact.name(), room.topic())
+
+  try {
+    room.add(contact)
+        .catch(e => {
+          log.error('Bot', 'room.add() exception: %s', e.stack)
+        })
+    setTimeout(_ => room.say('Welcome ', contact), 1000)
+  } catch (e) {
+    log.error('Bot', 'putInRoom() exception: ' + e.stack)
+  }
+}
+
+function getOutRoom(contact: Contact, room: Room) {
+  log.info('Bot', 'getOutRoom(%s, %s)', contact, room)
+
+  try {
+    room.say('You said "666" in my room, I will remove you out.')
+    room.del(contact)
+  } catch (e) {
+    log.error('Bot', 'getOutRoom() exception: ' + e.stack)
+  }
+}
+
+function getHelperContact() {
+  log.info('Bot', 'getHelperContact()')
+
+  // create a new room at least need 3 contacts
+  return Contact.find({ name: HELPER_CONTACT_NAME })
+}
+
+async function createDingRoom(contact): Promise<any> {
+  log.info('Bot', 'createDingRoom(%s)', contact)
+
+  try {
+    const helperContact = await getHelperContact()
+
+    if (!helperContact) {
+      log.warn('Bot', 'getHelperContact() found nobody')
+      return
+    }
+
+    log.info('Bot', 'getHelperContact() ok. got: %s', helperContact.name())
+
+    const contactList = [contact, helperContact]
+    log.verbose('Bot', 'contactList: %s', contactList.join(','))
+
+    const room = await Room.create(contactList, 'ding')
+    log.info('Bot', 'createDingRoom() new ding room created: %s', room)
+
+    room.topic('666 - created')
+
+    room.say('666 - created')
+
+    return room
+
+  } catch (e) {
+    log.error('Bot', 'getHelperContact() exception:', e.stack)
+    throw e
+  }
 }
